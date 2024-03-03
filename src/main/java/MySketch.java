6372bf2907d2205c7b258a50f66dbcf64e127aed
geist <julian.geist.2000@gmail.com>
2024-03-03 13:30:54 +0100

    fin

--- src/main/java/MySketch.java
+++ src/main/java/MySketch.java
@@ -5,19 +5,14 @@ import controlP5.*;
 
 public class MySketch extends PApplet {
 	ControlP5 cp5;
+	int seed = 0;
+	 float maxTranslationFactor = 0.7f;
+	float maxRotationFactor = 1f;
+	float intensity = 0.5f;
+
 	public void setup() {
 		this.getSurface().setResizable(true);
 		cp5 = new ControlP5(this);
-		cp5.addSlider("maxframe")
-				.setPosition(10, 10)
-				.setRange(1, 10)
-				.setValue(5)
-				.setSize(100, 20);
-		cp5.addSlider("decay")
-				.setPosition(10, 40)
-				.setRange(0.001f, 0.005f)
-				.setValue(5)
-				.setSize(100, 20);
 	}
 
 	public void settings() {
@@ -28,8 +23,29 @@ public class MySketch extends PApplet {
 
 	}
 
+
 	public void draw(){
-		background(64);
-		ellipse(mouseX, mouseY, 20, 20);
+		background(255);
+		float border = 30;
+		float rectSize = (width - 2 * border) / 12;
+		randomSeed(seed);
+
+
+		for (int row = 0; row < 24; row++) {
+			for (int column = 0; column < 12; column++) {
+				pushMatrix();
+				translate(border, border);
+				translate(column * rectSize, row * rectSize);
+				float weight = sin(0.003f * millis());
+				float translationFactor = weight * maxTranslationFactor;
+				float rotationFactor = weight * maxRotationFactor;
+
+				translate(translationFactor *  dist(mouseX,  mouseY, column * rectSize, row * rectSize)*random(0.5f, 0.5f),translationFactor /  dist(mouseX,  mouseY, column * rectSize, row * rectSize)*random(-0.5f, 0.5f));
+				rotate(rotationFactor *dist(mouseX,  mouseY, column * rectSize, row * rectSize)/dist(0, 0, width, height) * random(-1.f, 1.f));
+				rect(0, 0, rectSize, rectSize);
+				popMatrix();
+			}
+		}
+
 	}
 }
